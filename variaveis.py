427c4a041c1a4990bab79de6b7df807d9c70766e
Gabrielly Rodrigues <mariagabriellybrodrigues@gmail.com>
2026-01-20 12:29:50 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+# variaveis.py
+# Exemplo de variáveis e tipos de dados em Python
+
+nome = "Gabrielly"
+idade = 24
+altura = 1.73
+estudante = True
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("É estudante?", estudante)
